811430165d3cd635ab68dd204206f1e143de6d45
yinguilong <yinguilong@hotmail.com>
2018-11-05 21:51:01 +0800

    首次提交

---
+++ src/main/java/com/qushoping/opapi/OpapiApplication.java
@@ -0,0 +1,12 @@
+package com.qushoping.opapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpapiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/qushoping/opapi/OpapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qushoping.opapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
