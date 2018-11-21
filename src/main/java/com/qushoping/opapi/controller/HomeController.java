91a8d25b7aa412b97f3bf16eb47cb4a4e25c8910
yinguilong <yinguilong@hotmail.com>
2018-11-21 22:37:01 +0800

    新增swagger的支持

--- src/main/java/com/qushoping/opapi/controller/HomeController.java
+++ src/main/java/com/qushoping/opapi/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.qushoping.opapi.controller;
 
+import com.qushoping.opapi.pojo.user;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("用户相关")
 @RestController
 public class HomeController {
 
@@ -10,9 +16,16 @@ public class HomeController {
      *
      * 首页
      * */
-    @RequestMapping("/")
+    @ApiOperation("默认首页")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String Index() {
 
-        return  "helloWord";
+        return "helloWord";
+    }
+
+    @ApiOperation("添加用户")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public user Add(@RequestBody user u) {
+        return u;
     }
 }
--- src/main/java/com/qushoping/opapi/service/userServiceImp.java
+++ src/main/java/com/qushoping/opapi/service/userServiceImp.java
@@ -5,7 +5,9 @@ import com.qushoping.opapi.repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class userServiceImp implements userService {
@@ -24,6 +26,6 @@ public class userServiceImp implements userService {
 
     @Override
     public user findOne(Long id) {
-        return null;
+        return _userRepository.findById(id).orElse(null);
     }
 }
