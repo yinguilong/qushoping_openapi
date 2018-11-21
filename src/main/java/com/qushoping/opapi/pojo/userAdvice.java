bf9a9a42ff780850baa10ce7c350a99c6256bcb7
yinguilong <yinguilong@hotmail.com>
2018-11-21 22:56:10 +0800

    增加实体备注

--- src/main/java/com/qushoping/opapi/pojo/userAdvice.java
+++ src/main/java/com/qushoping/opapi/pojo/userAdvice.java
@@ -1,5 +1,20 @@
 package com.qushoping.opapi.pojo;
 
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@ApiModel("用户建议")
+@Entity
+@Setter
+@Getter
 public class userAdvice {
     private Long id;
+    private Integer status;
+    private String content;
+    private Long createTime;
+    private Long userId;
+    private Integer adviceLevel;
 }
